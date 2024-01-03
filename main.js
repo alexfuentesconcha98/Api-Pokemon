2934b595af13b1b9ec176d8503f6ee4009e5baf4
alexfuentesconcha98 <a.fuentes@duocuc.cl>
2024-01-03 19:56:43 -0300

    Se agrega un const para ocuparlo en la funcion y consumir el nombre

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const apiButton = document.getElementById('apiButton');
 const apiData = document.getElementById('apiData');
 const base_experience = document.getElementById('baseExperience'); 
+const namePokemon = document.getElementById('namePokemon');
 
 const callAPI = () => {
    fetch('https://pokeapi.co/api/v2/pokemon/ditto')
@@ -8,6 +9,7 @@ const callAPI = () => {
    .then(data => { 
       apiData.innerText = JSON.stringify(data);
       base_experience.innerText = `Experiencia base: ${JSON.stringify(data.base_experience)}`
+      namePokemon.innerText = `Nombre del pokemon: ${JSON.stringify(data.species.name)}`
    } )
    .catch(e => console.error(new Error(e)))
 }

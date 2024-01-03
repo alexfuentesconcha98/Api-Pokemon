ac1a66ee324f71fb6af5f29779d417d2bcf5270e
alexfuentesconcha98 <a.fuentes@duocuc.cl>
2024-01-03 19:44:51 -0300

    modifcando index.html y main.js

--- main.js
+++ main.js
@@ -1,9 +1,14 @@
 const apiButton = document.getElementById('apiButton');
+const apiData = document.getElementById('apiData');
+const base_experience = document.getElementById('baseExperience'); 
 
 const callAPI = () => {
    fetch('https://pokeapi.co/api/v2/pokemon/ditto')
    .then(res => res.json())
-   .then(data => console.log(data))
+   .then(data => { 
+      apiData.innerText = JSON.stringify(data);
+      base_experience.innerText = `Experiencia base: ${JSON.stringify(data.base_experience)}`
+   } )
    .catch(e => console.error(new Error(e)))
 }
 

ecf3cbb44d0b454fcb2bb59bfcc8134be36ea562
alexfuentesconcha98 <a.fuentes@duocuc.cl>
2024-01-03 18:27:00 -0300

    agregando catch error a main.js

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
 const apiButton = document.getElementById('apiButton');
 
 const callAPI = () => {
-   fetch('https://pokeapi.co/api/v2/pokemon/ditto').then(res =>{
-        console.log(res);
-   })
-};
+   fetch('https://pokeapi.co/api/v2/pokemon/ditto')
+   .then(res => res.json())
+   .then(data => console.log(data))
+   .catch(e => console.error(new Error(e)))
+}
 
 apiButton.addEventListener('click', callAPI);
